f0abea3213e93e91007442c7f62c648b29bec864
Giacomo-Draghi <jak127@byui.edu>
2021-04-27 18:43:50 +0200

    nodemon push

--- Week-1/Understanding the Basics/routes.js
+++ Week-1/Understanding the Basics/routes.js
@@ -38,7 +38,7 @@ const requestHandler = (req, res) => {
     res.setHeader('Content-Type', 'text/html');
     //Write multiple line of response
     res.write('<html>');
-    res.write('<head><title>My First Page</title><head>');
+    res.write('<head><title>My First Page</title></head>');
     res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
     res.write('</html>');
     res.end();
