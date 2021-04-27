f0de1d9c3c92c11dbe4f0e72f55edff4cfbe13a9
Giacomo-Draghi <jak127@byui.edu>
2021-04-27 19:46:46 +0200

    changes

---
+++ Week-2/Express.js-Problem/app.js
@@ -0,0 +1,21 @@
+// Adding modules
+const express = require('express'); 
+
+const app = express(); 
+
+// app.use('/product', (req, res, next) => {
+//     console.log('Product Page');
+//     res.send('<h1>Product Page</h1>');
+// });
+
+app.use('/users', (req, res, next) => {
+    console.log('Users Page');
+    res.send('<h1>Users Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+    console.log('Main Page');
+    res.send('<h1>Main Express Page</h1>');
+});
+
+app.listen(3000);
