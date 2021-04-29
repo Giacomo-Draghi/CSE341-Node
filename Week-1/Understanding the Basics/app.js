de94c4feee04c2e1449082036c1ec9e10273b91c
Giacomo-Draghi <jak127@byui.edu>
2021-04-30 00:46:55 +0200

    cleaning, adding ejs

--- Week-1/Understanding the Basics/app.js
+++ Week-1/Understanding the Basics/app.js
@@ -10,19 +10,20 @@ const bodyParser = require('body-parser');
 // Path
 const path = require('path');
 // handlebars
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 // Creating a express application
 const app = express();
 
 // telling to compile with templates and adding it
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/', 
-    defaultLayout: 'main-layout', 
-    extname: 'hbs'
-}));
+// app.engine('hbs', expressHbs({
+//     layoutsDir: 'views/layouts/', 
+//     defaultLayout: 'main-layout', 
+//     extname: 'hbs'
+// }));
 // app.set('view engine', 'pug');
-app.set('view engine', 'hbs');
+// app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views','views');
 
 // Working with the middleware
